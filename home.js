b6f3091508aa498d67a01325ef8b480b12f824ce
ARYAN KUMAR SINGH <98681477+aryansingh-123@users.noreply.github.com>
2023-04-04 23:25:02 +0530

    Button Functioning using Javascript

---
+++ home.js
@@ -0,0 +1,9 @@
+const buyNow = document.getElementById("buyNow");
+
+
+buyNow.addEventListener("click", function(){
+    console.log("button clicked");
+    window.location.href = 'paymentgateway.html';
+    
+}
+);

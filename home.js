9761e18e6dcbc9cd0448216acc5a494cf950e35b
ARYAN KUMAR SINGH <98681477+aryansingh-123@users.noreply.github.com>
2023-04-06 00:44:57 +0530

    added footer section and and image animation

--- home.js
+++ home.js
@@ -3,7 +3,8 @@ const buyNow = document.getElementById("buyNow");
 
 buyNow.addEventListener("click", function(){
     console.log("button clicked");
-    window.location.href = 'paymentgateway.html';
+    window.location.href = 'minor_project/login.html';
     
-}
-);
+});
+
+

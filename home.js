e55d259df09726a4d428aed3c54c5bad63585224
ARYAN KUMAR SINGH <98681477+aryansingh-123@users.noreply.github.com>
2023-04-06 01:57:37 +0530

    nothing just small changes i had done

--- home.js
+++ home.js
@@ -6,5 +6,17 @@ buyNow.addEventListener("click", function(){
     window.location.href = 'minor_project/login.html';
     
 });
+//Privacy Policy
+
+const privacyPolicy = document.getElementById("privacy");
+
+privacyPolicy.addEventListener("click", function(){
+    console.log("privacy policy used here ");
+    window.location.href = 'minor_project/privacy.html';
+
+});
+
+//Image Slider Carousel
+
 
 
